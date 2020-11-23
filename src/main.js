dfabcff346fffa4d7694a10033e0bb22d3369462
Mattias Bonte <wisecodr@gmail.com>
2020-11-23 19:11:50 +0100

    Add PWA Vue Module

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import { createApp } from 'vue';
 import './main.css';
 import App from './App.vue';
 import router from './router';
+import './registerServiceWorker'
 
 const app = createApp(App);
 
